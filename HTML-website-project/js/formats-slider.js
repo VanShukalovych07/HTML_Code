fa3149b6d793cbd462149d990ac260f25997bdc2
Vania <aktives685@gmail.com>
2023-11-10 22:26:07 +0200

    final website project

--- HTML-website-project/js/formats-slider.js
+++ HTML-website-project/js/formats-slider.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
@@ -11,12 +11,12 @@ const swiper = new Swiper('.swiper-container', {
     },
   },
   pagination: {
-    el: '.pagination',
-    bulletClass: 'pagination__button',
-    bulletActiveClass: 'pagination__button--active',
+    el: ".pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
   },
   navigation: {
-    nextEl: '.carousel-button.next',
-    prevEl: '.carousel-button.prev',
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
   },
 });
